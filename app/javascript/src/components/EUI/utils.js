afabc6d24dca942161e780d8791b92b59a9d7e1c
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-11-03 20:55:10 +0530

    Added cursor pointer for eui list

--- app/javascript/src/components/EUI/utils.js
+++ app/javascript/src/components/EUI/utils.js
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 
 export const buildListItemClassName = ({ article, selectedArticle }) =>
-  classNames("neeto-ui-font-medium", {
+  classNames("neeto-ui-font-medium cursor-pointer", {
     "neeto-ui-text-primary-600": selectedArticle.id === article.id,
     "neeto-ui-text-gray-600": selectedArticle.id !== article.id,
   });
