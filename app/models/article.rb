68e66dab7e5c049ee89495eee7732927caaf2a9c
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-11-03 09:39:13 +0530

    Added slug to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,11 +13,37 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }, format: { with: VALID_TITLE_REGEX }
   validates :body, presence: true
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
   before_save :update_last_published_at
+  before_create :set_slug
 
   private
 
     def update_last_published_at
       self.last_published_at = Time.zone.now if self.status_changed? && self.status == "Published"
+    end
+
+    def set_slug
+      title_slug = title.parameterize
+      latest_article_slug = Article.where(
+        "slug ~* ?",
+        "^#{title_slug}$|^#{title_slug}-[0-9]+$",
+      ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+      slug_count = 0
+      if latest_article_slug.present?
+        slug_count = latest_article_slug.split("-").last.to_i
+        only_one_slug_exists = slug_count == 0
+        slug_count = 1 if only_one_slug_exists
+      end
+      slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+      self.slug = slug_candidate
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, t("article.slug.immutable"))
+      end
     end
 end
