4267d5cf406f73fac4322a13dde645c51ec5106a
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-11-06 09:15:49 +0530

    Added cyclic redirection model validation

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -11,4 +11,28 @@ class Redirection < ApplicationRecord
   validates :to, presence: true, length: { maximum: MAX_LENGTH }
   # ,
   #  format: { with: VALID_TO_PATH_REGEX }
+  validate :to_and_from_not_equal
+  validate :check_redirection_loop
+
+  private
+
+    def to_and_from_not_equal
+      if self.to == self.from
+        errors.add(:base, "To and From paths cannot be equal")
+      end
+    end
+
+    def check_redirection_loop
+      if to_exist_in_from? && from_exist_in_to?
+        errors.add(:base, "This redirection causes redirection loop")
+      end
+    end
+
+    def to_exist_in_from?
+      Redirection.where(to: self.from).present?
+    end
+
+    def from_exist_in_to?
+      Redirection.where(from: self.to).present?
+    end
 end
