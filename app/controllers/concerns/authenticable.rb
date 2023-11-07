e399d4765f72ff3534dd23d98638aebadd4374cf
Lakshya Kanungo <89994423+lakshyakanungo@users.noreply.github.com>
2023-11-07 12:00:17 +0530

    Added failing eui controller tests (#133)

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -4,23 +4,17 @@ module Authenticable
   extend ActiveSupport::Concern
 
   included do
-    before_action :authenticate_user_using_x_auth_token
+    before_action :authenticate_using_x_auth_token
   end
 
   private
 
-    def authenticate_user_using_x_auth_token
+    def authenticate_using_x_auth_token
       auth_token = request.headers["X-Auth-Token"].presence
 
-      # puts "COMPARING "
-      # puts auth_token
-      # puts SiteSetting.first.authentication_token
-
       is_valid_token = ActiveSupport::SecurityUtils.secure_compare(
-        SiteSetting.first.authentication_token,
+        SiteSetting.first&.authentication_token,
         auth_token)
-
-      # puts "VALID REQUEST???", is_valid_token
 
       if SiteSetting.first.is_password_protected && !is_valid_token
         respond_with_error(t("unauthorised_request"), :forbidden)
