ffe858215a5a6fd5d88f190dc1c7ebcaa4678dbd
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-11-14 14:55:01 +0530

    Memoized current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,7 @@ class ApplicationController < ActionController::Base
   include ApiRescuable
   include Loggable
 
-  before_action :add_default_user
-
-  def add_default_user
-    @user = User.first
-  end
-
   def current_user
-    @user
+    @_current_user ||= User.first
   end
 end
