38574cd9ec0b8733d0d947b5d6b1b442c2614cc8
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-11-21 19:31:55 +0530

    Added remaining delete alerts

--- app/javascript/src/components/Dashboard/Articles/Page/Table/Dropdown.jsx
+++ app/javascript/src/components/Dashboard/Articles/Page/Table/Dropdown.jsx
@@ -1,44 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { MenuHorizontal } from "neetoicons";
 import { Dropdown as NeetoDropdown } from "neetoui";
 import { useTranslation } from "react-i18next";
 
-const Dropdown = ({ record, handleStatusChange, handleDelete }) => {
-  const { id, status } = record;
+import DeleteAlert from "./DeleteAlert";
 
-  const { Menu, MenuItem, Divider } = NeetoDropdown;
+const Dropdown = ({ record, handleStatusChange, handleDelete }) => {
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
 
   const { t } = useTranslation();
 
+  const { Menu, MenuItem, Divider } = NeetoDropdown;
+
+  const { id, status } = record;
+
   return (
-    <NeetoDropdown
-      appendTo={() => document.body}
-      buttonStyle="text"
-      className="w-6"
-      icon={() => <MenuHorizontal size={20} />}
-      strategy="fixed"
-    >
-      <Menu>
-        {status === "draft" ? (
+    <>
+      <NeetoDropdown
+        appendTo={() => document.body}
+        buttonStyle="text"
+        className="w-6"
+        icon={() => <MenuHorizontal size={20} />}
+        strategy="fixed"
+      >
+        <Menu>
+          {status === "draft" ? (
+            <MenuItem.Button
+              onClick={() => handleStatusChange({ id, status: "published" })}
+            >
+              {t("dashboard.articles.page.table.dropdown.publish")}
+            </MenuItem.Button>
+          ) : (
+            <MenuItem.Button
+              onClick={() => handleStatusChange({ id, status: "draft" })}
+            >
+              {t("dashboard.articles.page.table.dropdown.unpublish")}
+            </MenuItem.Button>
+          )}
+          <Divider />
           <MenuItem.Button
-            onClick={() => handleStatusChange({ id, status: "published" })}
+            style="danger"
+            onClick={() => setShowDeleteAlert(true)}
           >
-            {t("dashboard.articles.page.table.dropdown.publish")}
+            {t("dashboard.articles.page.table.dropdown.delete")}
           </MenuItem.Button>
-        ) : (
-          <MenuItem.Button
-            onClick={() => handleStatusChange({ id, status: "draft" })}
-          >
-            {t("dashboard.articles.page.table.dropdown.unpublish")}
-          </MenuItem.Button>
-        )}
-        <Divider />
-        <MenuItem.Button style="danger" onClick={() => handleDelete(id)}>
-          {t("dashboard.articles.page.table.dropdown.delete")}
-        </MenuItem.Button>
-      </Menu>
-    </NeetoDropdown>
+        </Menu>
+      </NeetoDropdown>
+      <DeleteAlert
+        handleDelete={handleDelete}
+        record={record}
+        setShowDeleteAlert={setShowDeleteAlert}
+        showDeleteAlert={showDeleteAlert}
+      />
+    </>
   );
 };
 
